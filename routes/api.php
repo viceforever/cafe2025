050b2def48bc41a1e3b388c9c786b7b83ad86fa3
Evgeniy S <teste@email.com>
2025-09-27 20:45:56 +0800

    LAST

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\DaDataController;
-=======
-use App\Http\Controllers\Api\AddressController;
->>>>>>> 0a2f531d9e689e1a73a43fa7763f980a2de195dc
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +10,6 @@ use App\Http\Controllers\Api\AddressController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-<<<<<<< HEAD
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
@@ -32,16 +27,3 @@ Route::get('/simple-test', [DaDataController::class, 'simpleTest'])->name('api.s
 Route::get('/dadata-test-config', [DaDataController::class, 'testConfig'])->name('api.dadata-test');
 Route::get('/dadata-test-api', [DaDataController::class, 'testApi'])->name('api.dadata-test-api');
 Route::get('/address-suggestions', [DaDataController::class, 'getAddressSuggestions'])->name('api.address-suggestions');
-=======
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/address/test', [AddressController::class, 'test'])->name('api.address.test');
-Route::post('/address/suggest', [AddressController::class, 'suggest'])->name('api.address.suggest');
->>>>>>> 0a2f531d9e689e1a73a43fa7763f980a2de195dc
